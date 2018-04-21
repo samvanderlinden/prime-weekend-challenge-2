1a290c1fc41894fff6ac4a97e005e310e6b7aade
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-21 12:50:11 -0500

    add numbers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,40 @@ console.log('client.js is sourced');
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('jquery is sourced')
+    console.log('jquery is sourced');
+    $('#addition').on('click', addition);
+    getAdditions();
+}
+
+// try using 'prepend' method to add row to top
+
+function addition() {
+    const newAdd = {
+        x: ($('#xInput').val()),
+        y: ($('#yInput').val()),
+        type: 'Add'
+    }
+    $.ajax({
+        method: 'post',
+        url: '/addition',
+        data: newAdd
+    })
+    .then(function(response) {
+        getAdditions();
+    })
+}
+
+function getAdditions() {
+    $.ajax({
+        method: 'GET',
+        url: '/get-addition',
+    })
+    .then(function(response){
+        console.log(response);
+        let xValue = Number($('#xInput').val());
+        let yValue = Number($('#yInput').val());
+        let sum = xValue + yValue;
+        $('#operationsHistory').empty();
+        $('#operationHistory').prepend('<p>' + xValue + ' + ' + yValue + ' = ' + sum + '</p>');
+    })
 }
--- server/server.js
+++ server/server.js
@@ -5,16 +5,29 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res){
-    console.log('yep it worked');
-    res.send('hey there');
+let calculations = [];
+
+// Addition requests
+
+app.post('/addition', function(req, res){
+    console.log(req.body);
+    calculations.push(req.body);
+    res.sendStatus(200);
+    console.log(calculations);
+});
+
+app.get('/get-addition', function(req, res){
+    res.send(calculations);
+    console.log(calculations);
 })
 
 
+// LISTENING
 app.listen(PORT, function (){ 
     console.log(`listening to port, ${PORT}`)
 });
